9b1175c37d08982db23b1ee1748c7e52793c46d4
Jesse Rosalia <jesse@Jesses-MacBook-Pro-2.local>
2016-12-10 15:49:17 -0800

    Initial Commit

---
+++ EvernoteTagHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EvernoteTagHelper
+//
+//  Created by Jesse Rosalia on 12/10/16.
+//  Copyright © 2016 Jesse Rosalia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EvernoteTagHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EvernoteTagHelper
+//
+//  Created by Jesse Rosalia on 12/10/16.
+//  Copyright © 2016 Jesse Rosalia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
